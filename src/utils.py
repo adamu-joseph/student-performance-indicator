f4cc2ac633f92c33b916ad3b9435b618ba2817b7
Adamu Joseph Ohigwere <ohigwerejosephadamu@gmail.com>
2026-04-27 10:50:42 +0100

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -50,6 +50,27 @@ class Variables:
         for col in self.df.select_dtypes(include=['float']):
             self.df[col] = pd.to_numeric(self.df[col], downcast='float')
 
+def evaluate_model(x_train, x_test, y_train, y_test, models):
+    try:
+        report = {}
+        
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+
+            model.fit(x_train, y_train) # Train the model
+            y_train_pred = model.predict(x_train)
+            y_test_pred = model.predict(x_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+    
+    except Exception as e:
+        raise CustomException(e, sys)
+     
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
